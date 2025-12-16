4d1a2d187f09135d44854d6ba098ef59942d37c0
Yuu0701 <148621629+Yuu0701@users.noreply.github.com>
2025-12-16 03:22:51 -0800

    tree log droppable after cutting

--- Assets/Scripts/Environment_Scripts/SpruceTreeScript.cs
+++ Assets/Scripts/Environment_Scripts/SpruceTreeScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SpruceTree : MonoBehaviour
 {
     public float health = 100f;
+    public GameObject treePrefab;
 
     // Call this when the tree takes damage
     public void TakeDamage(float damage)
@@ -19,5 +20,6 @@ public class SpruceTree : MonoBehaviour
         Destroy(gameObject);
 
         // play effects, animation, drop logs, etc.
+        Instantiate(treePrefab, transform.position, Quaternion.identity);
     }
 }
