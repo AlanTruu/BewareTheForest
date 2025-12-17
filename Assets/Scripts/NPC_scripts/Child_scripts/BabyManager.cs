36dca453cdbb3de4a5dae7bf981f11db850df880
Yuu0701 <148621629+Yuu0701@users.noreply.github.com>
2025-12-17 15:58:02 -0800

    fixed some code

--- Assets/Scripts/NPC_scripts/Child_scripts/BabyManager.cs
+++ Assets/Scripts/NPC_scripts/Child_scripts/BabyManager.cs
@@ -6,15 +6,25 @@ public class BabyManager : MonoBehaviour
 {
     public int totalMissingKids;
     private int kidAtBase = 0;
+    public TextMeshProUGUI Kidtext;
 
     public void KidReachedBase()
     {
         kidAtBase++;
+        UpdateText();
 
         // Show the win screen if all the kids are at the base
         if (kidAtBase >= totalMissingKids)
         {
             SceneManager.LoadScene("MainMenuScene");
+        }
+    }
+
+    void UpdateText()
+    {
+        if (Kidtext != null)
+        {
+            Kidtext.text = $"{kidAtBase}/{totalMissingKids} Children Found";
         }
     }
 }
